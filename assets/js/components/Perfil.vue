3bc37e573b629182d86fb5c6a99dbe1f32953fb8
Isaac Taveras <you@example.com>
2020-05-20 21:38:13 +0200

    Retoque animación en perfil

--- assets/js/components/Perfil.vue
+++ assets/js/components/Perfil.vue
@@ -1,81 +1,86 @@
 <template>
     <div class="mt-5 text-center">
         <b-container>
-         <b-card
-            header="Mi perfil"
-            header-text-variant="white"
-            header-tag="header"
-            header-bg-variant="info"
-            style="max-width: 40rem;"
+            <b-button variant="info" disabled v-if="!usuario.fechaNacimiento">
+                <b-spinner small type="grow"></b-spinner>
+                Cargando...
+            </b-button>
+            <b-card
+                header="Mi perfil"
+                header-text-variant="white"
+                header-tag="header"
+                header-bg-variant="info"
+                style="max-width: 40rem;"
+                v-if="usuario.fechaNacimiento"
             >
-            <h3>{{ usuario.nombre }} {{ usuario.apellidos }}</h3>
-            <b-row class="mb-2">
-                <b-col>
-                    <strong class="text-dark">Email:</strong>
-                </b-col>
-                <b-col>
-                    {{ usuario.email }}
-                </b-col>
-            </b-row>
+                <h3>{{ usuario.nombre }} {{ usuario.apellidos }}</h3>
+                <b-row class="mb-2">
+                    <b-col>
+                        <strong class="text-dark">Email:</strong>
+                    </b-col>
+                    <b-col>
+                        {{ usuario.email }}
+                    </b-col>
+                </b-row>
 
-            <b-row class="mb-2">
-                <b-col>
-                    <strong class="text-dark">Dni:</strong>
-                </b-col>
-                <b-col>
-                    {{ usuario.dni }}
-                </b-col>
-            </b-row>
+                <b-row class="mb-2">
+                    <b-col>
+                        <strong class="text-dark">Dni:</strong>
+                    </b-col>
+                    <b-col>
+                        {{ usuario.dni }}
+                    </b-col>
+                </b-row>
 
-            <b-row class="mb-2">
-                <b-col>
-                    <strong class="text-dark">Fecha de nacimiento:</strong>
-                </b-col>
-                <b-col>
-                    {{ usuario.fechaNacimiento.date.substring(0,10)}}
-                </b-col>
-            </b-row>
-            
-            <hr><span class="text-info">Dirección</span>
+                <b-row class="mb-2">
+                    <b-col>
+                        <strong class="text-dark">Fecha de nacimiento:</strong>
+                    </b-col>
+                    <b-col>
+                        {{ usuario.fechaNacimiento.date.substring(0,10)}}
+                    </b-col>
+                </b-row>
+                
+                <hr><span class="text-info">Dirección</span>
 
-            <b-row class="mb-2">
-                <b-col>
-                    <strong class="text-dark">Calle:</strong>
-                </b-col>
-                <b-col>
-                    {{ usuario.calle }}
-                </b-col>
-            </b-row>
+                <b-row class="mb-2">
+                    <b-col>
+                        <strong class="text-dark">Calle:</strong>
+                    </b-col>
+                    <b-col>
+                        {{ usuario.calle }}
+                    </b-col>
+                </b-row>
 
-            <b-row class="mb-2">
-                <b-col>
-                    <strong class="text-dark">Localidad:</strong>
-                </b-col>
-                <b-col>
-                    {{ usuario.localidad }}
-                </b-col>
-            </b-row>
+                <b-row class="mb-2">
+                    <b-col>
+                        <strong class="text-dark">Localidad:</strong>
+                    </b-col>
+                    <b-col>
+                        {{ usuario.localidad }}
+                    </b-col>
+                </b-row>
 
-            <b-row class="mb-2">
-                <b-col>
-                    <strong class="text-dark">Provincia:</strong>
-                </b-col>
-                <b-col>
-                    {{ usuario.provincia }}
-                </b-col>
-            </b-row>
+                <b-row class="mb-2">
+                    <b-col>
+                        <strong class="text-dark">Provincia:</strong>
+                    </b-col>
+                    <b-col>
+                        {{ usuario.provincia }}
+                    </b-col>
+                </b-row>
 
-            <b-row class="mb-2">
-                <b-col>
-                    <strong class="text-dark">Código postal:</strong>
-                </b-col>
-                <b-col>
-                    {{ usuario.cp }}
-                    <b-form-input v-model="usuario.cp" placeholder="Enter your name"></b-form-input>
-                    <b-button @click="editar">Editar</b-button>
-                </b-col>
-            </b-row>
-        </b-card>
+                <b-row class="mb-2">
+                    <b-col>
+                        <strong class="text-dark">Código postal:</strong>
+                    </b-col>
+                    <b-col>
+                        {{ usuario.cp }}
+                        <b-form-input v-model="usuario.cp"></b-form-input>
+                        <b-button @click="editar">Editar</b-button>
+                    </b-col>
+                </b-row>
+            </b-card>
         </b-container>
     </div>
 </template>
